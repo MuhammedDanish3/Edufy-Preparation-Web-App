0f93dd550682bb6e770da184a68b009ee6fd4010
Misbah-Waseem-007 <misbahwaseem435@gmail.com>
2024-03-19 19:11:46 +0500

    Sign up/Log in logics

--- First1.js
+++ First1.js
@@ -13,3 +13,21 @@ var swiper = new Swiper(".slide-content", {
         prevE1: ".swiper-button-prev",
     },
   });
+
+  // Get the button element
+const signupBtn = document.getElementById('signup-btn');
+
+// Add a click event listener to the button
+signupBtn.addEventListener('click', function() {
+    // Redirect to the signup page
+    window.location.href = 'signup.html'; // Change 'signup.html' to the path of your signup page
+});
+// Get the "Log in" text element by its ID
+const loginLink = document.getElementById('loginLink');
+
+// Add a click event listener to the "Log in" text
+loginLink.addEventListener('click', function() {
+    // Redirect the user to the login page
+    window.location.href = 'login.html';
+});
+
